8f617a273e137175ac031bf8df817b660632dbbc
sakshi013 <sakshibhat013@gmail.com>
2018-06-16 13:44:06 +0530

    toast

---
+++ app/src/main/java/com/example/sakshi/toast/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.sakshi.toast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "This is my toast in oncreat", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "This is my toast in onStart", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "This is my toast in onResume", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "This is my toast in pause", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "This is my toast in onStop", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "This is my toast in onDestroy", Toast.LENGTH_SHORT).show();
+
+    }
+}
